6fad74b1c70dcff40e0eecf43eb99f82fcd2997b
gaycodegal <sarahmavenoro@gmail.com>
2020-07-14 18:00:46 -0400

    main menu esc to quit + background color

--- puzzles/Menus/DebugMenu.gd
+++ puzzles/Menus/DebugMenu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _process(delta):
+	if Input.is_action_pressed("quit"):
+		get_tree().quit()
+
 func _on_01Skulls_pressed():
 	get_tree().change_scene("res://puzzles/puzzle_01.tscn")
 
