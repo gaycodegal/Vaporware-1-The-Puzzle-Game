7d0550a8207a95858180e14b86c9872d2eb8484f
gaycodegal <sarahmavenoro@gmail.com>
2020-07-22 19:57:39 -0400

    button grid can now do links

--- puzzles/05/ButtonGrid.gd
+++ puzzles/05/ButtonGrid.gd
@@ -1,4 +1,4 @@
-extends StaticBody
+extends MultiMeshInstance
 
 export(int) var cols = 1
 export(int) var rows = 1
@@ -6,31 +6,67 @@ export(float) var spacing = 1
 export(float) var mesh_width = 1 
 export(float) var mesh_height = 1
 export(float) var mesh_depth = 1
+export(Script) var button_script
+
 var space_width
 var space_height
 var width
 var height
 var multimeshinstance
-var collisionshape
+var last_pressed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	multimeshinstance = get_node("multimeshinstance")
-	collisionshape = get_node("collisionshape")
+	multimeshinstance = self
 	multimeshinstance.multimesh.instance_count = cols * rows
 	space_width = mesh_width + spacing
 	space_height = mesh_height + spacing
 	width = cols * mesh_width + spacing * (cols - 1)
 	height = rows * mesh_height + spacing * (rows - 1)
-	collisionshape.shape.extents = Vector3(width/2, height/2, mesh_depth/2)
+	var links = get_node("links")
+	links.rows = rows
+	links.cols = cols
+	links.spacing = spacing
+	links.mesh_width = mesh_width
+	links.mesh_height = mesh_height
+	links.mesh_depth = mesh_depth
+	links.space_width = space_width
+	links.space_height = space_height
+	links.width = width
+	links.height = height
+	var box_shape = BoxShape.new()
+	box_shape.extents = Vector3(mesh_width / 2, mesh_height / 2, mesh_depth / 2)
+	#collisionshape.shape.extents = Vector3(width/2, height/2, mesh_depth/2)
 	for col in range(cols):
 		for row in range(rows):
 			var index = col + row * cols
 			var pos = Transform()
-			pos = pos.translated(Vector3(col * space_width - width / 2 + mesh_width / 2, row * space_height - height / 2 + mesh_height/2, 0))
+			pos = pos.translated(Vector3(
+				col * space_width - width / 2 + mesh_width / 2,
+				row * space_height - height / 2 + mesh_height/2, 
+				0))
 			multimeshinstance.multimesh.set_instance_transform(index, pos)
+			var static_body = StaticBody.new()
+			static_body.transform = pos
+			static_body.name = "%d : %d" % [row, col]
+			static_body.collision_layer = 2
+			static_body.collision_mask = 2
+			var collision_shape = CollisionShape.new()
+			collision_shape.shape = box_shape
+			static_body.add_child(collision_shape)
+			static_body.set_script(button_script)
+			static_body.row = row
+			static_body.col = col
+			self.add_child(static_body)
+			
 
+func distance_ok(a, b):
+	return abs(a[0]-b[0]) + abs(a[1]-b[1]) <= 1
 
-func interact(player):
-	var point: Vector3 = player.get_collision_point()
-	print(to_local(point))
+func interact(row, col, player):
+	var current = [row, col]
+	if last_pressed != null:
+		if not distance_ok(last_pressed, current) or not get_node("links").link(last_pressed, current):
+			current = null
+		
+	last_pressed = current
---
+++ puzzles/05/links.gd
@@ -0,0 +1,48 @@
+extends MultiMeshInstance
+var cols = 1
+var rows = 1
+var spacing = 1
+var mesh_width = 1 
+var mesh_height = 1
+var mesh_depth = 1
+var space_width
+var space_height
+var width
+var height
+var used = {}
+var positions = []
+
+func _ready():
+	positions = []
+	used = {}
+	multimesh.instance_count = 0
+
+func point_str(point):
+	return "%d:%d" % point
+
+func pair_str(a, b):
+	a = point_str(a)
+	b = point_str(b)
+	var pair = [a, b]
+	pair.sort()
+	return pair
+
+func link(last, current):
+	var key = pair_str(last, current)
+	if key in used:
+		return false
+	used[key] = true
+	var row = float(last[0] + current[0]) / 2
+	var col = float(last[1] + current[1]) / 2
+	var index = multimesh.instance_count
+	multimesh.instance_count = index + 1
+	var pos = Transform()
+	var vpos = Vector3(
+		col * space_width - width / 2 + mesh_width / 2,
+		row * space_height - height / 2 + mesh_height/2, 
+		0)
+	pos = pos.translated(vpos)
+	positions.append(pos)
+	for i in range(len(positions)):
+		multimesh.set_instance_transform(i, positions[i])
+	return true
